ad1cdb2c44c68ae3e587cf9a63f6ad5747a6744f
Контакт Патруль <98600734+7x7x49@users.noreply.github.com>
2025-10-02 13:52:51 +0300

    Update RestTest.java
    Update RestTest.java

--- src/test/java/org/ibs/RestTest.java
+++ src/test/java/org/ibs/RestTest.java
@@ -16,14 +16,14 @@ public class RestTest {
 
     @BeforeAll
     static void openFood() {
-
         RestAssured.baseURI = "http://localhost:8080";
         try {
             given()
                     .when()
-                    .get("/").
-                    then().
-                    statusCode(200);
+                    .get("/")
+                    .then()
+                    .statusCode(200);
+            System.out.println("-Сервер доступен-");
         } catch (Exception e) {
             System.out.println("Сервер недоступен! Запустите qualit-sandbox.jar");
         }
@@ -31,17 +31,26 @@ public class RestTest {
 
     @Test
     void testFoodAPI() {
+        // Получаем начальный список товаров
+        printYellow("✭ Начальный список товаров:");
+        getAllFood();
+
+        printYellow("✭ Добавляем товары...");
+
         // Корректные значения
         testAddFood("Редиска", "VEGETABLE", false);
         testAddFood("Банан", "FRUIT", true);
+
         // Пустые значения
         testAddFood("", "VEGETABLE", false);
         testAddFood("Апельсин", "", true);
         testAddFood("", "", true);
+
         // Наименование латиницей
         testAddFood("Banana", "FRUIT", true);
         testAddFood("Cucumber", "VEGETABLE", false);
         testAddFood("Radish", "VEGETABLE", true);
+
         // Использование цифр, пробелов и символов в Наименовании
         testAddFood("77", "VEGETABLE", false);
         testAddFood("Банан29", "FRUIT", true);
@@ -50,33 +59,56 @@ public class RestTest {
         testAddFood("?? 3335Баnan", "FRUIT", false);
         testAddFood(" 12245Баnan??", "FRUIT", true);
         testAddFood("!?;?*;№", "FRUIT", true);
+
         // Тип кириллицей
         testAddFood("Банан", "Фрукт", true);
         testAddFood("Огурец", "овощ", false);
+
+        // Получаем финальный список товаров
+        printYellow("✭ Финальный список товаров:");
+        getAllFood();
     }
 
     void testAddFood(String name, String type, boolean exotic) {
-        // Реализуем добавление данных: Наименование (поле), Тип (выпадающий список), Экзотический (чек-бокс)
+        // Добавляем товары
         String jsonBody = "{\"name\": \"" + name + "\", \"type\": \"" + type + "\", \"exotic\": " + exotic + "}";
 
         Response response = given()
-                .contentType("application/json") // Помечаем, что данные JSON
+                .contentType("application/json") // Помечаем, что данные в JSON
                 .body(jsonBody) // Данные JSON
                 .when()
-                .post("/api/food"); // Отправляем запрос
+                .post("/api/food");
 
-        // Если продукт успешно добавлен
-        if (response.getStatusCode() == 200) {
-            printGreen(name + " | " + type + " | " + exotic + " успешно добавлен");
-            printBlue("Статус код: " + response.getStatusCode());
+        int statusCode = response.getStatusCode();
+
+        if (statusCode == 200) {
+            printGreen("Успешно добавлены: " + name + " | " + type + " | " + exotic);
+            printBlue("   Статус код: " + statusCode);
+        } else {
+            printRed("Не добавлены: " + name + " | " + type + " | " + exotic);
+            printPurple("   Ответ сервера: " + response.getBody().asString());
+            printBlue("   Статус код: " + statusCode);
         }
-        // Если продукт не добавлен
-        else {
-            printRed("======================================");
-            printRed(name + " | " + type + " | " + exotic + " не добавлен");
-            printPurple("Ответ сервера: " + response.getBody().asString());
-            printBlue("Статус код: " + response.getStatusCode());
-            printRed("======================================");
+    }
+
+    void getAllFood() {
+        // Получаем список товаров
+        Response response = given()
+                .when()
+                .get("/api/food");
+
+        if (response.getStatusCode() == 200) {
+            String responseBody = response.getBody().asString();
+
+            if (responseBody.contains("[]")) {
+                printBlue("Список товаров пуст.");
+            } else {
+                printCyan("Список товаров:");
+                printCyan(responseBody);
+            }
+        } else {
+            printRed("Ошибка при получении списка товаров!");
+            printRed("   Статус код: " + response.getStatusCode());
         }
     }
 
@@ -84,16 +116,29 @@ public class RestTest {
     void printGreen(String text) {
         System.out.println("\u001B[32m" + text + "\u001B[0m");
     }
+
     // Метод для красного текста
     void printRed(String text) {
         System.out.println("\u001B[31m" + text + "\u001B[0m");
     }
+
     // Метод для фиолетового текста
     void printPurple(String text) {
         System.out.println("\u001B[35m" + text + "\u001B[0m");
     }
+
     // Метод для голубого цвета
     void printBlue(String text) {
         System.out.println("\u001B[36m" + text + "\u001B[0m");
+    }
+
+    // Метод для желтого цвета
+    void printYellow(String text) {
+        System.out.println("\u001B[33m" + text + "\u001B[0m");
+    }
+
+    // Метод для бирюзового цвета
+    void printCyan(String text) {
+        System.out.println("\u001B[96m" + text + "\u001B[0m");
     }
 }
